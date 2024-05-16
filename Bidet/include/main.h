9a424f59008ddb735fc9d5b1243d73fda6960923
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2024-05-16 17:54:46 -0400

    Fixed debounce

--- Bidet/include/main.h
+++ Bidet/include/main.h
@@ -2,6 +2,7 @@
 
 #define IR_PIN 3
 #define PUMP_PIN 2
+#define NUMBER_REMOTE_COUNT 9
 
 void handleInput(uint16_t command);
 bool contains(uint16_t value);
--- Bidet/src/main.cpp
+++ Bidet/src/main.cpp
@@ -2,35 +2,27 @@
 
 #include <IRremote.hpp>
 
-uint16_t numberCommands[] = { 12, 24, 94, 8, 28, 90, 66, 82, 74}; // 9
+uint16_t numberCommands[NUMBER_REMOTE_COUNT] = { 12, 24, 94, 8, 28, 90, 66, 82, 74}; // 9
 
 bool isPumpRunning = false;
 
-const int debounceDelay = 1000;
-long lastPressTime = 0;
-
 void setup() {
-    Serial.begin(9600);
     IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
 }
 
 void loop() {
-    long currentTime = millis();
-    bool isDebouncing = currentTime - lastPressTime < debounceDelay;
-    if (IrReceiver.decode() && !isDebouncing) {
-        Serial.println(IrReceiver.decodedIRData.command);
+    if (IrReceiver.decode()) {
         handleInput(IrReceiver.decodedIRData.command);
         
+        delay(300);
         IrReceiver.resume();
-        lastPressTime = currentTime;
     }
 }
 
 void handleInput(uint16_t command) {
     switch (command)
     {
-        case 69:
-            Serial.println("Toggle command");   
+        case 69: // Power
             isPumpRunning = !isPumpRunning;
             digitalWrite(PUMP_PIN, isPumpRunning ? HIGH : LOW);
             break;
@@ -39,12 +31,10 @@ void handleInput(uint16_t command) {
             if (isPumpRunning || !contains(command)) 
                 return;
             
-            for (int i = 0; i < 9; i++) {
-                if (command == numberCommands[i]) {
-                    Serial.println("Number command: " + String(i));
-                    
+            for (int i = 0; i < NUMBER_REMOTE_COUNT; i++) {
+                if (command == numberCommands[i]) {                    
                     digitalWrite(PUMP_PIN, HIGH);
-                    delay(i * 10000);
+                    delay((i + 1) * 10000);
                     digitalWrite(PUMP_PIN, LOW);
                     isPumpRunning = false;
                     break;
@@ -55,9 +45,8 @@ void handleInput(uint16_t command) {
 }
 
 bool contains(uint16_t value) {
-    for (int i = 0; i < 9; i++) { // nine is the count of the array
+    for (int i = 0; i < NUMBER_REMOTE_COUNT; i++) { 
         if (numberCommands[i] == value) {
-            Serial.println("Found value in array");
             return true;
         } 
     }
