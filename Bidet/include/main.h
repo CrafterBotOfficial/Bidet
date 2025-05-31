88f0900a2611bd4eda6b2ceb8507c0e59ca06c73
CrafterbotOfficial <jackchupp@icloud.com>
2025-05-31 18:36:22 -0400

    Cleaned up project

--- Bidet/include/main.h
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-
-#define IR_PIN 3
-#define PUMP_PIN 2
-#define NUMBER_REMOTE_COUNT 9
-
-void handleInput(uint16_t command);
-bool contains(uint16_t value);
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include "IRremote.h"
+
+#define PUMP_PIN 2
+#define IR_PIN 3
+
+#define POWER_BTN 69 
+#define TIME_BTN_MULTIPLIER 1000 
+
+int timedCommands[] = { 12, 24, 94, 8, 28, 90, 66, 82, 74}; 
+
+bool pumpState;
+int remoteInputCount = sizeof(timedCommands) / sizeof(timedCommands[0]);
+
+void setup() {
+    IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
+    pinMode(PUMP_PIN, OUTPUT);
+    digitalWrite(PUMP_PIN, LOW);
+}
+
+void loop() {
+    if (!IrReceiver.decode()) return;
+    IrReceiver.resume();
+
+    int *commandPtr = &IrReceiver.decodedIRData.command;
+    if (*commandPtr == POWER_BTN) {
+        pumpState = !pumpState;
+        digitalWrite(PUMP_PIN, pumpState);    
+        goto end;
+    }
+    
+    if (!pumpState)
+        for (int index = 0; index < remoteInputCount; index ++) {
+            if (timedCommands[index] != *commandPtr) continue;
+            digitalWrite(PUMP_PIN, HIGH);
+            delay((index + 1) * TIME_BTN_MULTIPLIER);
+            digitalWrite(PUMP_PIN, LOW);
+            break;
+        }
+
+end:
+    delay(300);
+}
